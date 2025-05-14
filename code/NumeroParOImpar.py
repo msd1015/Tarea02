00abef16de0d29d4cd1e707e8b60b19787742158
Maria Sabadell <msd1015@alu.ubu.es>
2025-05-14 11:47:46 +0200

    Creación de carpetas y configuración de ficheros

---
+++ code/NumeroParOImpar.py
@@ -0,0 +1,8 @@
+# Solicitar un número al usuario
+numero = int(input("Introduce un número: "))
+
+# Verificar si el número es par o impar
+if numero % 2 == 0:
+    print(f"El número {numero} es par.")
+else:
+    print(f"El número {numero} es impar.")
